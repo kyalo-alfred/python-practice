72e8a10bf039af439adf82cf95552b26b6070013
kyalo-alfred <kyalo.alfred@strathmore.edu>
2025-04-25 20:17:32 +0300

    Check whether a value is even or odd

--- even or odd.py
+++ even or odd.py
@@ -1,5 +1,9 @@
+#Create an input function
 number = int(input("Enter a number: "))
 
+#Use if condition with the help of a modulus
+# to check whether a number is divisible by 2.
+# Thus knowing whether it is even or odd
 if number % 2 == 0:
  print("Even")
 else:
