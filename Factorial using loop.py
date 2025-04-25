2174ba89b7fb399edff9cfd8c775178d0264e863
kyalo-alfred <kyalo.alfred@strathmore.edu>
2025-04-25 20:29:53 +0300

    Factorial using loop

--- Factorial using loop.py
+++ Factorial using loop.py
@@ -1,10 +1,14 @@
+#I first made an input function for data input
 number = int(input("Enter a number: "))
+
+#I then created the variables fact and i to aid in the loop
 fact =1
 i=1
 
+#Created a while loop and the variables fact and i for each iteration
 while i <= number:
-  fact *= i
+  fact = fact * i
   i += 1
 
-
+#Finalized by displaying the output
 print(f"Your factorial of:{number} is {fact}")
