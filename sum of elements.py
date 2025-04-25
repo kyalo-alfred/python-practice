e1f60489ac5a3e49aa7c399b852eb281017660e9
kyalo-alfred <kyalo.alfred@strathmore.edu>
2025-04-25 15:29:53 +0300

    sum of elements in a list

--- sum of elements.py
+++ sum of elements.py
@@ -1,3 +1,6 @@
 values=[1,2,3,4,5,6,7,8,9]
+#I first created a list named values
 total=sum(values)
+#I then created a variable total and used the sum function to add the elements in the list values.
 print(total)
+#Finally I printed the output of the sum of values by printing the Function total.
